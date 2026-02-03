229be36a12e5a4b2fe4f9c7fb1dfb879a3612298
Casey Lazik <clazik@uw.edu>
2026-02-03 14:57:38 -0800

    Starting on getting final resume data, look over

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ STAT 496 Project
 
 Authors - Casey Lazik, Harry Schuckman
 
-Project Idea: What affects resume evaluation? - Give the LLM many resumes varied in certain
-ways(our variables we are testing) and ask it to rate the strength of the resume or if it would
-recommend it or some way of evaluating it. Could vary job descriptions/industries as well.
+Project: What affects resume evaluation? - Give the LLM many resumes varied on certain
+their contents and ask it to rate the strength of the resume or if it would
+recommend it or some way of evaluating it. We generate our resumes using LLMs.
 
 
-We can create resumes such that we are able to control for variables, and avoid potential privacy issues.
+We create resumes such that we are able to control for variables, and avoid potential privacy issues.
 
 
 This project also analyzes racial biases present in LLMs, which are used by companies when deciding candidates for job positions
---
+++ generateResumes.py
@@ -0,0 +1,63 @@
+
+
+'''
+These are the variables we will vary for all resumes (across all job prompts)
+'''
+
+# Harry get the names
+names = 
+
+years_of_college = [0, 1, 2, 3, 4, 5, 6] # Instead of degree earned 
+
+experience = ["No prior experience in relevant field", "A single Internship in the field", 
+              "Multiple Internships in relevant field", "Prior Job experience in the field", "Research done in relevant field"]
+
+
+
+
+input_resume_template = """
+Generate a resume for the following candidate.
+
+
+
+"""
+
+
+
+
+
+
+
+
+jobs = ["Actuary", "Software Engineer", "Retail Salesperson", "Nurse", "High School Teacher"]
+
+
+
+
+
+
+
+actuarial_skills = []
+
+software_engineer_skills = []
+
+retail_salesperson_skills = []
+
+nurse_skills = []
+
+high_school_teacher_skills = []
+
+
+skills =[actuarial_skills, software_engineer_skills, retail_salesperson_skills, nurse_skills, high_school_teacher_skills]
+
+
+
+
+for i in range(len(jobs)):
+    job = jobs[i]
+    skill = skills[i]
+
+
+
+
+    
