3ec5fd852f84b0b7229ae3b28287d1f8f23fbe85
harry247928 <hschuck@uw.edu>
2026-02-17 18:33:50 -0800

    Update readOutput.py
    Added a logistic regression

--- readOutput.py
+++ readOutput.py
@@ -1,4 +1,6 @@
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
+import numpy as np
 
 data_path = "Output/evaluations.csv"
 
@@ -18,3 +20,34 @@ print(pd.crosstab(df["race"], df["recommendation"]))
 print(pd.crosstab(df["sex"], df["recommendation"]))
 
 print(df["recommendation"].unique()) # Check if any response didn't follow directions
+
+
+df["recommendation"] = df["recommendation"].map({"top candidate": 3, "good candidate": 2,
+                                                 "weak candidate but pass": 1, "Dismiss initial screening": 0})
+
+
+X = pd.get_dummies(df["race"], drop_first=True)
+y = df["recommendation"]
+
+model = LogisticRegression(penalty='none', solver="lbfgs")
+model.fit(X, y)
+
+for name, coef in zip(X.columns, model.coef_[0]):
+    print(name, coef)
+
+print("\nOdds Ratios:")
+for name, coef in zip(X.columns, model.coef_[0]):
+    print(name, np.exp(coef))
+
+
+X = pd.get_dummies(df["sex"], drop_first=True)
+
+model = LogisticRegression(penalty='none', solver="lbfgs")
+model.fit(X, y)
+
+for name, coef in zip(X.columns, model.coef_[0]):
+    print("\n", name, coef)
+
+print("\nOdds Ratios:")
+for name, coef in zip(X.columns, model.coef_[0]):
+    print(name, np.exp(coef))
