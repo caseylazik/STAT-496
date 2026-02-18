102200a06b33c443a014177b085c424a5e51fd59
Casey Lazik <clazik@uw.edu>
2026-02-18 14:16:52 -0800

    fix

--- readOutput.py
+++ readOutput.py
@@ -18,3 +18,5 @@ print(pd.crosstab(df["race"], df["recommendation"]))
 print(pd.crosstab(df["sex"], df["recommendation"]))
 
 print(df["recommendation"].unique()) # Check if any response didn't follow directions
+
+print(df["years_attended"].unique()) # Check if any response didn't follow directions
