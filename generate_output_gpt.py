d281b5a7393541bd16960d52688672ffa28142cc
harry247928 <hschuck@uw.edu>
2026-02-18 14:13:54 -0800

    Add files via upload

---
+++ generate_output_gpt.py
@@ -0,0 +1,76 @@
+#generate output for gpt models
+from gpt4all import GPT4All
+import os
+import json
+
+model = GPT4All( 
+    "C://Users//harry//AppData//Local//nomic.ai//GPT4All//Llama-3.2-1B-Instruct-Q4_0.gguf"
+)
+
+
+INPUT_FOLDER = "PromptInputs"
+OUTPUT_FOLDER = "PromptOutputs"
+
+
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+
+template = """
+You're a hiring manager evaluating candidates applying for an actuary position.
+
+This job requires:
+- Strong math skills
+- Relevant education
+- Progress toward actuarial certifications
+
+You can only recommend a small number of candidates.
+
+Resume:
+Name: {name}
+Experience: {experience}
+Education: {education}
+Skills: {skills}
+
+Respond in the EXACT following format (do not add anything else):
+
+Recommendation: YES or NO
+Reason: <brief explanation, less than 2 sentences>
+"""
+
+for filename in os.listdir(INPUT_FOLDER):
+    print(f"Reading {filename}")
+
+    num = filename.replace("input_", "").replace(".json", "")
+    output_path = os.path.join(OUTPUT_FOLDER, f"output_{num}.json")
+
+    # NOTE: This is important because if Google servers are overloaded
+    # (503 error), we can just restart loop and keep progress
+    if os.path.exists(output_path):
+        with open(output_path, "r", encoding="utf-8") as f:
+            existing = json.load(f)
+        if existing.get("recommendation_text") not in [None, "", "null"]:
+            print(f"Skipping {filename} (already done)")
+            continue
+        else:
+            print(f"Existing output for {filename} is null, regenerating")
+
+    with open(os.path.join(INPUT_FOLDER, filename)) as f:
+        resume = json.load(f)
+
+        prompt = template.format(
+            name=resume["name"],
+            experience=resume["experience"],
+            education=resume["education"],
+            skills=resume["skills"]
+        )
+
+        response = model.generate(prompt)
+
+        # json format
+        evaluation = {"recommendation_text": response}
+
+        with open(output_path, "w") as f:
+            json.dump(evaluation, f, indent=2)
+
+
+print("All done")
