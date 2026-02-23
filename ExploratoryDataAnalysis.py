f508f7cc2a7f5cc490462c49bc7c9684de8d2a2a
Casey Lazik <clazik@uw.edu>
2026-02-23 10:07:29 -0800

    started on finalReport (just copied your draft for now and a bit of formatting)

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,8 +1,31 @@
-from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 
 data_path = "Output/evaluations.csv"
+
+
+
+df = pd.read_csv(data_path)
+
+
+print(df["recommendation"].value_counts())
+
+
+
+
+print(pd.crosstab(df["race"], df["recommendation"]))
+
+
+
+print(df["recommendation"].unique()) # Check if any response didn't follow directions
+
+
+df["recommendation"] = df["recommendation"].map({"top candidate": 3, "good candidate": 2,
+                                                 "weak candidate but pass": 1, "Dismiss initial screening": 0})
+
+
+print("Now lets do experience required prompt")
+data_path = "Output/evaluationsExperienceRequired.csv"
 
 
 
@@ -24,3 +47,5 @@ print(df["recommendation"].unique()) # Check if any response didn't follow direc
 
 df["recommendation"] = df["recommendation"].map({"top candidate": 3, "good candidate": 2,
                                                  "weak candidate but pass": 1, "Dismiss initial screening": 0})
+
+
