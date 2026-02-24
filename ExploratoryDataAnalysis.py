0f13323718f84b0ccd5d6604d7790fd35d67564c
Casey Lazik <clazik@uw.edu>
2026-02-24 14:25:20 -0800

    starting EDA

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,51 +1,66 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-data_path = "Output/evaluations.csv"
-
-
-
-df = pd.read_csv(data_path)
-
-
-print(df["recommendation"].value_counts())
+NORMAL_PATH = "Output/evaluations.csv"
+EXPERIENCE_REQ_PATH = "Output/evaluationsExperienceRequired.csv"
 
 
-
-
-print(pd.crosstab(df["race"], df["recommendation"]))
-
-
-
-print(df["recommendation"].unique()) # Check if any response didn't follow directions
-
-
-df["recommendation"] = df["recommendation"].map({"top candidate": 3, "good candidate": 2,
-                                                 "weak candidate but pass": 1, "Dismiss initial screening": 0})
-
-
-print("Now lets do experience required prompt")
-data_path = "Output/evaluationsExperienceRequired.csv"
-
-
-
-df = pd.read_csv(data_path)
-
-
-print(df["recommendation"].value_counts())
+# Normal prompt (no experience required)
+df_normal = pd.read_csv(NORMAL_PATH)
+# Experience Required prompt ( 
+df_exp_req = pd.read_csv(EXPERIENCE_REQ_PATH)
 
 
 
 
-print(pd.crosstab(df["race"], df["recommendation"]))
+recommendation_numeric_mapping = {
+    "Dismiss initial screening": 0,
+    "weak candidate but pass": 1,
+    "good candidate": 2,
+    "top candidate": 3
+}
 
 
-print(pd.crosstab(df["sex"], df["recommendation"]))
 
-print(df["recommendation"].unique()) # Check if any response didn't follow directions
+df_normal["recommendation_score"] = df_normal["recommendation"].map(recommendation_numeric_mapping)
 
-
-df["recommendation"] = df["recommendation"].map({"top candidate": 3, "good candidate": 2,
-                                                 "weak candidate but pass": 1, "Dismiss initial screening": 0})
+df_exp_req["recommendation_score"] = df_exp_req["recommendation"].map(recommendation_numeric_mapping)
 
 
+
+
+
+print("\nOriginal/Normal Prompt: \n")
+
+print(df_normal["recommendation"].value_counts())
+
+print(pd.crosstab(df_normal["race"], df_normal["recommendation"]))
+
+
+
+print(df_normal.groupby("race")["recommendation_score"].sum())
+
+
+print(df_normal.groupby("sex")["recommendation_score"].sum())
+ 
+
+
+
+
+
+print("\n\nNow lets do experience required prompt: \n")
+
+
+
+print(df_exp_req["recommendation"].value_counts())
+
+print(pd.crosstab(df_exp_req["race"], df_exp_req["recommendation"]))
+
+
+
+print(df_exp_req.groupby("race")["recommendation_score"].sum())
+
+
+print(df_exp_req.groupby("sex")["recommendation_score"].sum())
